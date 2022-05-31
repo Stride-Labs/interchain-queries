f46230e18905575d53a473e9dbb55bb55222cb91
Riley Edmunds <riley@stridelabs.co>
2022-05-30 20:50:10 -0400

    add stride and gaia testnet configs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -102,8 +102,11 @@ func defaultConfig(keyHome string, debug bool) []byte {
 	return Config{
 		DefaultChain: "cosmoshub",
 		Chains: map[string]*client.ChainClientConfig{
-			"cosmoshub": client.GetCosmosHubConfig(keyHome, debug),
-			"osmosis":   client.GetOsmosisConfig(keyHome, debug),
+			"cosmoshub":      client.GetCosmosHubConfig(keyHome, debug),
+			"osmosis":        client.GetOsmosisConfig(keyHome, debug),
+			"stride-testnet": client.GetStrideTestnetConfig(keyHome, debug),
+			"gaia-testnet":   client.GetGaiaTestnetConfig(keyHome, debug),
+			// "stride": clien
 		},
 	}.MustYAML()
 }
